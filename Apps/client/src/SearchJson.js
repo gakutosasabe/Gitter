db848ae5ae9e480fd7addb122e261e386563f03c
gakuto45 <gakuto0122@gmail.com>
2023-03-06 18:22:54 +0900

    JSONから日付ごとの練習結果をだす配列を作る（途中，同じ日で繰り返し練習した場合は，足してあげる処理をしていない）

--- Apps/client/src/SearchJson.js
+++ Apps/client/src/SearchJson.js
@@ -4,20 +4,38 @@ class JsonSearch{
 
     //Jsonから[日付,練習時間[分]]の配列にして返す
     jsonToDateArray(data){
+        var last_time;
+        var last_status;
+        let array = [[]];
+
         json.forEach(obj =>  {
             const status = obj.practice_status;
             const strtime = obj.time;
             const time = newDate(strtime);
+            if(last_status == "START" && status == "END"){
+                //時間の差分を出す
+                var diff = time.getTime() - last_time.getTime();
+                //分に直す
+                var diff_minutes = Math.abs(diff) / (60 * 1000);
+                var date = time.getFullYear()
+                + '/' + ('0' + (time.getMonth() + 1)).slice(-2)
+                + '/' + ('0' + time.getDate()).slice(-2);
+
+                //配列にPush
+                array.push([date,diff_minutes]);
 
 
-            
+            }else{
+                last_time = time;
+                last_status = status;
+            }
         });
 
 
 
     
         
-        return Array;
+        return array;
     }
 
 }
